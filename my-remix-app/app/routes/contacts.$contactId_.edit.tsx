0b2dc1185a8c50c3853fd62642a665c140ee0f1d
michaelroring <roringmichael@gmail.com>
2024-03-23 02:09:27 +0700

    feat: add simple app

---
+++ my-remix-app/app/routes/contacts.$contactId_.edit.tsx
@@ -0,0 +1,86 @@
+/*
+Note the weird _ in $contactId_. By default, routes will automatically nest inside routes with the same prefixed name. Adding a trailing _ tells the route to not nest inside app/routes/contacts.$contactId.tsx
+*/
+
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { getContact, updateContact } from "../data";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+};
+
+export default function EditContact() {
+  const { contact } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+
+  return (
+    <Form key={contact.id} id="contact-form" method="post">
+      <p>
+        <span>Name</span>
+        <input
+          defaultValue={contact.first}
+          aria-label="First name"
+          name="first"
+          type="text"
+          placeholder="First"
+        />
+        <input
+          aria-label="Last name"
+          defaultValue={contact.last}
+          name="last"
+          placeholder="Last"
+          type="text"
+        />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input
+          defaultValue={contact.twitter}
+          name="twitter"
+          placeholder="@jack"
+          type="text"
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          aria-label="Avatar URL"
+          defaultValue={contact.avatar}
+          name="avatar"
+          placeholder="https://example.com/avatar.jpg"
+          type="text"
+        />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea defaultValue={contact.notes} name="notes" rows={6} />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        {/* 🧐 Why is there no event.preventDefault() on the button? A <button type="button">, while seemingly redundant, is the HTML way of preventing a button from submitting its form.
+         */}
+
+        <button onClick={() => navigate(-1)} type="button">
+          Cancel
+        </button>
+      </p>
+    </Form>
+  );
+}
